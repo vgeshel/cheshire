b40a488a4160fab4a0e71b28a2c739fd0436a1c4
Vadim Geshel <vadim@yummly.com>
2015-08-31 16:35:40 -0700

    upgrade jackson

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject com.vgeshel/cheshire "5.3.1.1"
+(defproject com.vgeshel/cheshire "5.3.1.2"
   :description "JSON and JSON SMILE encoding, fast."
   :url "https://github.com/dakrone/cheshire"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"
             :distribution :repo}
   :warn-on-reflection false
-  :dependencies [[com.fasterxml.jackson.core/jackson-core "2.3.1"]
-                 [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.3.1"]
+  :dependencies [[com.fasterxml.jackson.core/jackson-core "2.6.1"]
+                 [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.6.1"]
                  [tigris "0.1.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                                   [org.clojure/test.generative "0.1.4"]
